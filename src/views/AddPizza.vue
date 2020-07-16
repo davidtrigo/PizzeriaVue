afc209ef4437873ef7dbaf5ddf0ca3bc733cfa48
davidtrigo <d.trigo83@gmail.com>
2020-07-16 21:01:22 +0200

    create views add edit

---
+++ src/views/AddPizza.vue
@@ -0,0 +1,5 @@
+<template>
+  <div >
+    <h1>Add Pizza</h1>
+  </div>
+</template>
---
+++ src/views/EditPizza.vue
@@ -0,0 +1,5 @@
+<template>
+  <div >
+    <h1>Editando pizza {{ $route.params.idPizza }}</h1>
+  </div>
+</template>
