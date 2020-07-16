755d2bebf29cf4b311713f06b072e9f22881e60b
davidtrigo <d.trigo83@gmail.com>
2020-07-16 20:56:29 +0200

    changes names  links

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Pizzas</router-link> |
-      <router-link to="/about">Ingredients</router-link>
+      <router-link to="/ingredients">Ingredients</router-link>
     </div>
     <router-view/>
   </div>
