c2bf5266953e9638f3332d96d2025f0534895898
davidtrigo <d.trigo83@gmail.com>
2020-07-16 14:15:38 +0200

    add Component PizzaList and index.js for routes

---
+++ src/components/PizzaList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Listado de Pizzas</h1>
+    <div v-auth = "'admin'">
+     Usuario Admin
+    </div>
+ 
+  </div>
+</template>
+
+<script>
+import {AuthDirective} from '../userlib/AuthDirective';
+
+export default {
+  name: 'PizzaList',
+  directives:{
+    AuthDirective
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Pizzas from '../views/Pizzas.vue'
+import guard from '../userlib/UserGuardService'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Pizzas',
+    component: Pizzas
+  },
+  {
+    path: '/ingredients',
+    name: 'Ingredients',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "ingredients" */ '../views/Ingredients.vue'),
+    meta:{
+      role:'admin'
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "register" */ '../views/Register.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+//si estas autorizado (por ejem ingredientes) accede, sino te redirige a login
+router.beforeEach(async (to, from, next) => {
+  const { role } = to.meta || {}
+  const isAuthorize = await guard(role)
+  if (!isAuthorize) {
+    next({ name: 'Login' })
+  }
+  else {
+    next();
+  }
+
+})
+export default router
